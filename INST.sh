d471e8d0c7eab9021688ec915825fae940c003f3
Jun Wook Heo <47840618+JunwookHeo@users.noreply.github.com>
2021-08-16 10:50:44 +1000

    Update INST.sh

--- INST.sh
+++ INST.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-sudo amazon-linux-extras install -y epel
+echo "Start Initialization"
 
+echo "Installing epel"
+sudo amazon-linux-extras install -y epel
+if [ $? - eq 0 ]
+then
+    echo "Installation success"
+else
+    echo "Installation error"
+fi
+
+echo "Installing mosquitto"
 sudo yum -y install mosquitto
+if [ $? - eq 0 ]
+then
+    echo "Installation success"
+else
+    echo "Installation error"
+fi
