1b163c4f6181834e86fea28a5b128051ef85f1b9
Jun Wook Heo <47840618+JunwookHeo@users.noreply.github.com>
2021-09-07 18:39:32 +1000

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -22,7 +22,7 @@ def readFromServer():
 ## Read data from csv file
 import csv
 def readFromFile():
-	with open('weather.csv') as csv_file:
+	with open('weather1.csv') as csv_file:
 		csv_reader = csv.reader(csv_file, delimiter=',')
 		line_count = 0
 		for row in csv_reader:
